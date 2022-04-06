fd0c60501a259abb55627e9b1c0d13919bf427c9
Mina Hanna <53411598+mhanna15@users.noreply.github.com>
2022-04-05 17:40:18 -0700

    Upload functionality (#21)
    * added progress for upload
    
    * added fields for sermon data and functionality for adding document to firestore
    
    * Update pages/uploader.tsx
    
    Co-authored-by: Youssef Asaad <youssef.a.asaad@gmail.com>
    
    * added current date as default
    
    * Update pages/uploader.tsx
    
    Co-authored-by: Youssef Asaad <youssef.a.asaad@gmail.com>
    
    * added new date type, uuid assignments, and clear form after upload functionality
    
    * made it requiered for title, speaker, and date fields
    
    Co-authored-by: Youssef Asaad <youssef.a.asaad@gmail.com>

--- pages/uploader.tsx
+++ pages/uploader.tsx
@@ -1,150 +1,286 @@
 /**
  * Page for uploaders to use to upload, trim, and add intro/outro to audio file
  */
- import type { NextPage } from 'next';
- import Footer from '../components/Footer';
- import Navbar from '../components/Navbar';
- import AudioTrimmer from '../components/AudioTrimmer';
- import styles from '../styles/Uploader.module.css';
- // import firebase from '../firebase/firebase';
- // import { getFirestore, collection, addDoc } from 'firebase/firestore';
- import { useCallback, useState } from 'react';
- import { FileError, FileRejection, useDropzone } from 'react-dropzone';
- 
- import { ref, uploadBytes } from 'firebase/storage';
- import { storage } from '../firebase/firebase';
- import { getAuth } from 'firebase/auth';
- 
- export interface UploadableFile {
-   file: File;
-   name: string;
-   preview: string;
-   errors: FileError[];
- }
- // TODO: figure out how to type this properly
- // let Url: {
- //   new (url: string | URL, base?: string | URL | undefined): URL;
- //   createObjectURL: any;
- //   prototype?: URL;
- //   revokeObjectURL?: (url: string) => void;
- // };
- let Url: any;
- if (typeof window !== 'undefined') {
-   Url = window.URL || window.webkitURL;
- }
- const Uploader: NextPage = () => {
-   // const db = getFirestore(firebase);
-   const [file, setFile] = useState<UploadableFile>();
-   const onDrop = useCallback(
-     (acceptedFiles: File[], rejectedFiles: FileRejection[]) => {
-       // Do something with the files
-       if (rejectedFiles.length > 0) {
-         // console.log(rejectedFiles[0].errors[0]);
-         setFile(undefined);
-         return;
-       }
-       // const reader = new FileReader();
-       // reader.readAsDataURL(acceptedFiles[0]);
-       // reader.addEventListener('progress', function (pe) {
-       //   if (pe.lengthComputable) {
-       //     console.log('Progress:', pe.loaded, 'Total:', pe.total);
-       //   }
-       // });
-       // reader.addEventListener(
-       //   'load',
-       //   function () {
-       const mappedAccepted = {
-         file: acceptedFiles[0],
-         // preview: reader.result as string,
-         preview: Url.createObjectURL(acceptedFiles[0]),
-         name: acceptedFiles[0].name.replace(/\.[^/.]+$/, ''),
-         errors: [],
-       };
-       setFile(mappedAccepted);
-       //   },
-       //   false
-       // );
-       // const mappedAccepted2 = acceptedFiles.map((file) => ({
-       //   file,
-       //   errors: [],
-       // }));
-       // setFile((curr) => [...curr, mappedAccepted, ...rejectedFiles]);
-     },
-     []
-   );
- 
-   const { getRootProps, getInputProps } = useDropzone({
-     onDrop,
-     multiple: false,
-     accept: ['audio/*'],
-   });
- 
-   function uploadFile(file: File) {
-     const sermonRef = ref(storage, `sermons/${file.name}`);
-     getAuth();
-     uploadBytes(sermonRef, file).then((snapshot) => {});
-     // console.log('Upload File', file);
-     // const url = '...';
-     // return await new Promise((res, rej) => {
-     //   const xhr = new XMLHttpRequest();
-     //   xhr.open('POST', url);
-     //   xhr.onload = () => {
-     //     res('00')
-     //   }
-     //   xhr.onerror = (evt) => rej(evt);
-     //   xhr.upload.onprogress = (event) => {
-     //     if (event.lengthComputable) {
-     //       const percentage = (event.loaded / event.total) * 100;
-     //       onProgress(Math.round(percentage))
-     //     }
-     //   };
-     //   const formData = new FormData();
-     //   formData.append('file', file);
-     //   formData.append('key', key)
-     //   xhr.send(formData)
-     // });
-   }
- 
-   return (
-     <div className={styles.container}>
-       <Navbar />
-       <h1>Uploader</h1>
-       <form className={styles.form}>
-         {file ? (
-           <>
-             <AudioTrimmer url={file.preview}></AudioTrimmer>
-             <button
-               type="button"
-               className={styles.button}
-               onClick={() => setFile(undefined)}
-             >
-               Clear
-             </button>
-           </>
-         ) : (
-           <div className={styles.dragAndDrop} {...getRootProps()}>
-             <input type="hidden" {...getInputProps()} />
-             <p>
-               Drag &apos;n&apos; drop audio files here, or click to select files
-             </p>
-           </div>
-         )}
-         <input
-           className={styles.button}
-           // type="submit"
-           value="Upload"
-           disabled={file === undefined}
-           onClick={() => {
-             if (file !== undefined) {
-               uploadFile(file.file);
-             }
-           }}
-         />
-       </form>
-       <Footer />
-     </div>
-   );
- };
- 
- export default Uploader;
- 
+import type { NextPage } from 'next';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import AudioTrimmer from '../components/AudioTrimmer';
+
+import styles from '../styles/Uploader.module.css';
+// import firebase from '../firebase/firebase';
+// import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { useCallback, useState } from 'react';
+import { FileError, FileRejection, useDropzone } from 'react-dropzone';
+
+import { ref, uploadBytesResumable } from 'firebase/storage';
+import { firebase, storage } from '../firebase/firebase';
+import { getAuth } from 'firebase/auth';
+import {
+  collection, // eslint-disable-line
+  addDoc, // eslint-disable-line
+  getFirestore,
+  setDoc,
+  doc,
+} from 'firebase/firestore';
+
+import { v4 as uuidv4 } from 'uuid';
+
+export interface UploadableFile {
+  file: File;
+  name: string;
+  preview: string;
+  errors: FileError[];
+}
+// TODO: figure out how to type this properly
+// let Url: {
+//   new (url: string | URL, base?: string | URL | undefined): URL;
+//   createObjectURL: any;
+//   prototype?: URL;
+//   revokeObjectURL?: (url: string) => void;
+// };
+let Url: any;
+if (typeof window !== 'undefined') {
+  Url = window.URL || window.webkitURL;
+}
+const Uploader: NextPage = () => {
+  getAuth();
+
+  const db = getFirestore(firebase);
+  const [file, setFile] = useState<UploadableFile>();
+  const [uploadProgress, setUploadProgress] = useState<string>();
+
+  const [title, setTitle] = useState<string>('');
+  const [subtitle, setSubtitle] = useState<string>('');
+  const [date, setDate] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [speaker, setSpeaker] = useState<string>('');
+  const [scripture, setScripture] = useState<string>('');
+  const [topic, setTopic] = useState<string>('');
+
+  const onDrop = useCallback(
+    (acceptedFiles: File[], rejectedFiles: FileRejection[]) => {
+      // Do something with the files
+      if (rejectedFiles.length > 0) {
+        // console.log(rejectedFiles[0].errors[0]);
+        setFile(undefined);
+        return;
+      }
+      // const reader = new FileReader();
+      // reader.readAsDataURL(acceptedFiles[0]);
+      // reader.addEventListener('progress', function (pe) {
+      //   if (pe.lengthComputable) {
+      //     console.log('Progress:', pe.loaded, 'Total:', pe.total);
+      //   }
+      // });
+      // reader.addEventListener(
+      //   'load',
+      //   function () {
+      const mappedAccepted = {
+        file: acceptedFiles[0],
+        // preview: reader.result as string,
+        preview: Url.createObjectURL(acceptedFiles[0]),
+        name: acceptedFiles[0].name.replace(/\.[^/.]+$/, ''),
+        errors: [],
+      };
+      setFile(mappedAccepted);
+      //   },
+      //   false
+      // );
+      // const mappedAccepted2 = acceptedFiles.map((file) => ({
+      //   file,
+      //   errors: [],
+      // }));
+      // setFile((curr) => [...curr, mappedAccepted, ...rejectedFiles]);
+    },
+    []
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    multiple: false,
+    accept: ['audio/*'],
+  });
+
+  const clearForm = () => {
+    setFile(undefined);
+    setTitle('');
+    setSubtitle('');
+    setDate('');
+    setDescription('');
+    setSpeaker('');
+    setScripture('');
+    setTopic('');
+  };
+
+  function uploadFile(file: File) {
+    const id = uuidv4();
+    const sermonRef = ref(storage, `sermons/${id}`);
+    // const sermonRef = ref(storage, `sermons/${file.name}`);
+    const uploadTask = uploadBytesResumable(sermonRef, file);
+    const sermonData = {
+      title: title,
+      subtitle: subtitle,
+      date: date,
+      description: description,
+      speaker: speaker,
+      scripture: scripture,
+      topic: topic.split(' '),
+      ref: sermonRef.fullPath,
+    };
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(`${Math.round(progress)}%`);
+        switch (snapshot.state) {
+          case 'paused':
+            break;
+          case 'running':
+            break;
+        }
+      },
+      (error) => {
+        setUploadProgress(`Error: ${error}`);
+      },
+      () => {
+        // With autogenerated id
+        // addDoc(collection(db, 'sermons'), sermonData);
+
+        // with custom id so it doesnt duplicate upload
+        setDoc(doc(db, 'sermons', id), sermonData);
+        clearForm();
+        setUploadProgress('Uploaded!');
+      }
+    );
+
+    // console.log('Upload File', file);
+    // const url = '...';
+    // return await new Promise((res, rej) => {
+    //   const xhr = new XMLHttpRequest();
+    //   xhr.open('POST', url);
+    //   xhr.onload = () => {
+    //     res('00')
+    //   }
+    //   xhr.onerror = (evt) => rej(evt);
+    //   xhr.upload.onprogress = (event) => {
+    //     if (event.lengthComputable) {
+    //       const percentage = (event.loaded / event.total) * 100;
+    //       onProgress(Math.round(percentage))
+    //     }
+    //   };
+    //   const formData = new FormData();
+    //   formData.append('file', file);
+    //   formData.append('key', key)
+    //   xhr.send(formData)
+    // });
+  }
+
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <h1>Uploader</h1>
+      <form>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="required"
+          />
+        </label>
+        <label>
+          Subtitle:
+          <input
+            type="text"
+            value={subtitle}
+            onChange={(e) => setSubtitle(e.target.value)}
+          />
+        </label>
+        <label>
+          Date:
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+        <label>
+          Description:
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+        <label>
+          Speaker:
+          <input
+            type="text"
+            value={speaker}
+            onChange={(e) => setSpeaker(e.target.value)}
+            placeholder="required"
+          />
+        </label>
+        <label>
+          Scripture:
+          <input
+            type="text"
+            value={scripture}
+            onChange={(e) => setScripture(e.target.value)}
+          />
+        </label>
+        <label>
+          Topic:
+          <input
+            type="text"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          />
+        </label>
+      </form>
+      <form className={styles.form}>
+        {file ? (
+          <>
+            <AudioTrimmer url={file.preview}></AudioTrimmer>
+            <button
+              type="button"
+              className={styles.button}
+              onClick={() => {
+                setFile(undefined);
+                setUploadProgress(undefined);
+              }}
+            >
+              Clear
+            </button>
+          </>
+        ) : (
+          <div className={styles.dragAndDrop} {...getRootProps()}>
+            <input type="hidden" {...getInputProps()} />
+            <p>
+              Drag &apos;n&apos; drop audio files here, or click to select files
+            </p>
+          </div>
+        )}
+        <input
+          className={styles.button}
+          type="button"
+          value="Upload"
+          disabled={
+            file === undefined || title === '' || date === '' || speaker === ''
+          }
+          onClick={() => {
+            if (file !== undefined) {
+              uploadFile(file.file);
+            }
+          }}
+        />
+        <p>{uploadProgress}</p>
+      </form>
+      <Footer />
+    </div>
+  );
+};
+
+export default Uploader;
