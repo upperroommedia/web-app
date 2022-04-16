f20ca09612d77cf95507b4a39728568a5d1901c2
Mina Hanna <53411598+mhanna15@users.noreply.github.com>
2022-04-15 21:23:54 -0700

    have working speaker list, still have to fetch speakers

---
+++ components/SpeakersList.tsx
@@ -0,0 +1,25 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import Select from 'react-select';
+
+interface speakerListProps {
+  setSpeaker: Dispatch<SetStateAction<string>>;
+}
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
+const SpeakersList = (props: speakerListProps) => {
+  return (
+    <Select
+      options={options}
+      onChange={(e) => {
+        if (e !== null) props.setSpeaker(e.value);
+      }}
+    />
+  );
+};
+
+export default SpeakersList;
