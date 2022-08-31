7deba0a15dfe1fa4b4dccf84e067e92dfb7e62f6
Youssef Asaad <youssef.a.asaad@gmail.com>
2022-08-31 03:23:05 -0400

    Backend functions (#29)
    * file uploader init
    
    * temp save
    
    * functions init
    
    * will revoke the credentials
    
    * remove env
    
    * audio preview playback
    
    * url type
    
    * custom audio trimmer ui
    
    * move handles
    
    * fixed safari dropzone issuze
    
    * can upload to storage
    
    * Upload functionality (#21)
    
    * added progress for upload
    
    * added fields for sermon data and functionality for adding document to firestore
    
    * Update pages/uploader.tsx
    
    Co-authored-by: Youssef Asaad <youssef.a.asaad@gmail.com>
    
    * added current date as default
    
    * Update pages/uploader.tsx
    
    Co-authored-by: Youssef Asaad <youssef.a.asaad@gmail.com>
    
    * added new date type, uuid assignments, and clear form after upload functionality
    
    * made it requiered for title, speaker, and date fields
    
    Co-authored-by: Youssef Asaad <youssef.a.asaad@gmail.com>
    
    * fixed uuid issue
    
    * inital endpoint for uploading audio to subsplash
    
    * init api endpoint for uploading to subsplash
    
    * working endpoints but need refactoring
    
    Co-authored-by: Mina Hanna <53411598+mhanna15@users.noreply.github.com>

---
+++ pages/signIn.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { firebase } from '../firebase/firebase';
+import { getAuth, EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import styles from '../styles/SignIn.module.css';
+const auth = getAuth(firebase);
+
+const uiConfig = {
+  signInSuccessUrl: '/',
+  signInOptions: [
+    EmailAuthProvider.PROVIDER_ID,
+    GoogleAuthProvider.PROVIDER_ID,
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  ],
+};
+
+const SignInScreen: NextPage = () => {
+  return (
+    <div className={`${styles.container} ${styles.center}`}>
+      <h1 className={styles.header}>Welcome to Upper Room Media</h1>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+    </div>
+  );
+};
+
+export default SignInScreen;
