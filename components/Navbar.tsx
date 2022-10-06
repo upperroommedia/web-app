92b761cde9a4bb315ce64151337e3275393561ff
Youssef Asaad <youssef.a.asaad@gmail.com>
2022-10-05 23:56:51 -0500

    added profile pic and display name to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,47 +9,60 @@ import useAuth from '../context/user/UserContext';
 
 const Navbar: FunctionComponent = () => {
   const { user } = useAuth();
-
+  const displayName = user?.displayName || user?.email;
+  const photoSrc = user?.photoURL || '/user.png';
   return (
     <nav>
       <div className={styles.navbar}>
-        <h1 className={styles.title}>Upper Room Media</h1>
-        <Image src="/upper_room_media_icon.png" alt="Upper Room Media Logo" width="100%" height="100%" />
+        <div className={styles.navbar_header_container}>
+          <h1 className={styles.title}>Upper Room Media</h1>
+          <Image src="/upper_room_media_icon.png" alt="Upper Room Media Logo" width="100%" height="100%" />
+        </div>
+        {user && (
+          <div className={styles.user_info}>
+            <div className={styles.profile_pic}>
+              <Image src={photoSrc} layout="fill"></Image>
+            </div>
+            <p>{displayName}</p>
+          </div>
+        )}
       </div>
-      <Link href="/">
-        <a className={styles.nav_link}>Home</a>
-      </Link>
-      <Link href="/sermons">
-        <a className={styles.nav_link}>Sermons</a>
-      </Link>
-      <Link href="/about">
-        <a className={styles.nav_link}>About</a>
-      </Link>
+      <div className={styles.navbar_links}>
+        <Link href="/">
+          <a className={styles.nav_link}>Home</a>
+        </Link>
+        <Link href="/sermons">
+          <a className={styles.nav_link}>Sermons</a>
+        </Link>
+        <Link href="/about">
+          <a className={styles.nav_link}>About</a>
+        </Link>
 
-      {!user ? (
-        <>
-          <Link href="/login">
-            <a className={styles.nav_link}>Login</a>
-          </Link>
-          <Link href="/signup">
-            <a className={styles.nav_link}>Sign Up</a>
-          </Link>
-        </>
-      ) : (
-        // All Pages that need to be protected
-        <>
-          {user?.role === 'admin' ? (
-            <Link href="/uploader">
-              <a className={styles.nav_link}>Uploader</a>
+        {!user ? (
+          <>
+            <Link href="/login">
+              <a className={styles.nav_link}>Login</a>
             </Link>
-          ) : (
-            <></>
-          )}
-          <Link href="/logout">
-            <a className={styles.nav_link}>Log out</a>
-          </Link>
-        </>
-      )}
+            <Link href="/signup">
+              <a className={styles.nav_link}>Sign Up</a>
+            </Link>
+          </>
+        ) : (
+          // All Pages that need to be protected
+          <>
+            {user?.role === 'admin' ? (
+              <Link href="/uploader">
+                <a className={styles.nav_link}>Uploader</a>
+              </Link>
+            ) : (
+              <></>
+            )}
+            <Link href="/logout">
+              <a className={styles.nav_link}>Log out</a>
+            </Link>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
