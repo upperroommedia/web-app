147c2c1c773975ab4546b39d6264503cd05c9259
Youssef Asaad <youssef.a.asaad@gmail.com>
2022-10-11 19:38:53 -0400

    Added package analyzer and fixed some mui imports (#45) (#47)
    * Added package analyzer and fixed some mui imports
    
    To run the package analyzer run pnpm analyze
    
    * fixed next config and added some dynamic imports
    
    * added sharp for next images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
     formats: ['image/avif', 'image/webp'],
   },
-};
-
+});
 module.exports = nextConfig;
