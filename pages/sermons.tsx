f68b277a5c090921e718e5d1a47c78e713906bef
abakirh <abakirhanna@gmail.com>
2022-10-11 12:01:29 -0400

    fixed nothing

--- pages/sermons.tsx
+++ pages/sermons.tsx
@@ -4,24 +4,13 @@
 import type { GetServerSideProps, NextPage } from 'next';
 // import PropTypes from 'prop-types';
 
-import SermonListCard from '../components/SermonListCard';
-import BottomAudioBar from '../components/BottomAudioBar';
-
 import { Sermon } from '../types/Sermon';
-
-import { useEffect } from 'react';
-import useAudioPlayer from '../context/audio/audioPlayerContext';
 
 interface Props {
   sermons: Sermon[];
 }
 
 const Sermons: NextPage<Props> = ({ sermons }: Props) => {
-  const { playing, playlist, setPlaylist, currentSermon, currentSecond } = useAudioPlayer();
-
-  useEffect(() => {
-    setPlaylist(sermons);
-  }, []);
 
   // const handleSermonClick = (sermon: Sermon) => {
   //   // console.log('handle click');
@@ -41,33 +30,9 @@ const Sermons: NextPage<Props> = ({ sermons }: Props) => {
             // gap: '3px',
           }}
         >
-          {playlist.map((sermon, i) => {
-            const key = `sermon_list_card_${i}`;
-            if (currentSermon.key === sermon.key) {
-              return (
-                <SermonListCard
-                  sermon={{ ...sermon, currentSecond }}
-                  playing={playing}
-                  key={key}
-                  playlist={playlist}
-                  setPlaylist={setPlaylist}
-                />
-              );
-            } else {
-              return (
-                <SermonListCard
-                  sermon={sermon}
-                  playing={false}
-                  key={key}
-                  playlist={playlist}
-                  setPlaylist={setPlaylist}
-                />
-              );
-            }
-          })}
+          {sermons}
         </div>
       </div>
-      {currentSermon && <BottomAudioBar />}
     </>
   );
 };
