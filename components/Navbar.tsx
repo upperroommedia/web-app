eddee4bd2df34e7bf05c4b134180d3950b76bf30
Mina Hanna <53411598+mhanna15@users.noreply.github.com>
2022-11-08 18:33:45 -0800

    checkpoint for cropping image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/Navbar.module.css';
 import useAuth from '../context/user/UserContext';
+import ImageUploader from './ImageUploader';
 
 const Navbar: FunctionComponent = () => {
   const { user } = useAuth();
@@ -13,6 +14,7 @@ const Navbar: FunctionComponent = () => {
   const photoSrc = user?.photoURL || '/user.png';
   return (
     <nav>
+      <ImageUploader />
       <div className={styles.navbar}>
         <div className={styles.navbar_header_container}>
           <h1 className={styles.title}>Upper Room Media</h1>
