7df2808f1c4a3dcecc61b4f13f3b7ed6b1c38973
Mina Hanna <53411598+mhanna15@users.noreply.github.com>
2022-11-10 18:15:09 -0800

    added features to speaker table

--- components/SpeakerTable.tsx
+++ components/SpeakerTable.tsx
@@ -1,6 +1,6 @@
+import dynamic from 'next/dynamic';
 import { ChangeEvent, useState } from 'react';
 import Image from 'next/image';
-import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -13,18 +13,51 @@ import TableSortLabel from '@mui/material/TableSortLabel';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
 import { ISpeaker } from '../types/Speaker';
+import { visuallyHidden } from '@mui/utils';
 
 type Order = 'asc' | 'desc';
 
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort(array: ISpeaker[], order: Order) {
-  return order === 'asc'
-    ? array.sort((a, b) => a.name.localeCompare(b.name))
-    : array.sort((a, b) => b.name.localeCompare(a.name));
-}
+const stableSort = (array: ISpeaker[], order: Order, orderBy: keyof ISpeaker) => {
+  if (orderBy === 'name') {
+    return order === 'asc'
+      ? array.sort((a, b) => a.name.localeCompare(b.name))
+      : array.sort((a, b) => b.name.localeCompare(a.name));
+  } else if (orderBy === 'listId') {
+    return order === 'asc'
+      ? array.sort((a, b) => {
+          if (a.listId && !b.listId) {
+            return 1;
+          } else if (b.listId && !a.listId) {
+            return -1;
+          } else return 0;
+        })
+      : array.sort((a, b) => {
+          if (a.listId && !b.listId) {
+            return -1;
+          } else if (b.listId && !a.listId) {
+            return 1;
+          } else return 0;
+        });
+  } else if (orderBy === 'images') {
+    return order === 'asc'
+      ? array.sort((a, b) => {
+          if (a.images && !b.images) {
+            return 1;
+          } else if (b.images && !a.images) {
+            return -1;
+          } else return 0;
+        })
+      : array.sort((a, b) => {
+          if (a.images && !b.images) {
+            return -1;
+          } else if (b.images && !a.images) {
+            return 1;
+          } else return 0;
+        });
+  }
+  return array;
+};
 
 interface HeadCell {
   disablePadding: boolean;
@@ -37,33 +70,32 @@ const headCells: readonly HeadCell[] = [
   {
     id: 'name',
     numeric: false,
-    disablePadding: true,
+    disablePadding: false,
     label: 'Name',
   },
   {
     id: 'listId',
-    numeric: true,
+    numeric: false,
     disablePadding: false,
     label: 'List Id',
   },
   {
     id: 'images',
-    numeric: true,
+    numeric: false,
     disablePadding: false,
     label: 'Images',
   },
 ];
 
 interface SpeakerTableProps {
-  numSelected: number;
   onRequestSort: (event: any, property: keyof ISpeaker) => void;
-  onSelectAllClick: (event: ChangeEvent<HTMLInputElement>) => void;
   order: Order;
+  orderBy: string;
   rowCount: number;
 }
 
-function SpeakerTableHead(props: SpeakerTableProps) {
-  const { onSelectAllClick, order, numSelected, rowCount, onRequestSort } = props;
+const SpeakerTableHead = (props: SpeakerTableProps) => {
+  const { order, orderBy, onRequestSort } = props;
   const createSortHandler = (property: keyof ISpeaker) => (event: any) => {
     onRequestSort(event, property);
   };
@@ -71,102 +103,62 @@ function SpeakerTableHead(props: SpeakerTableProps) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all speakers',
-            }}
-          />
-        </TableCell>
         {headCells.map((headCell) => (
-          <TableCell key={headCell.id} align={headCell.numeric ? 'right' : 'left'}>
-            {headCell.id === 'name' ? (
-              <TableSortLabel
-                active={true}
-                direction={order === 'asc' ? 'asc' : 'desc'}
-                onClick={createSortHandler(headCell.id)}
-              >
-                {headCell.label}
-              </TableSortLabel>
-            ) : (
-              headCell.label
-            )}
+          <TableCell key={headCell.id} sortDirection={orderBy === headCell.id ? order : false}>
+            <TableSortLabel
+              onClick={createSortHandler(headCell.id)}
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
           </TableCell>
         ))}
       </TableRow>
     </TableHead>
   );
-}
+};
 
-interface SpeakerTableToolbarProps {
-  numSelected: number;
-}
-
-function SpeakerTableToolbar(props: SpeakerTableToolbarProps) {
-  const { numSelected } = props;
-
+const SpeakerTableToolbar = () => {
   return (
     <Toolbar
       sx={{
         pl: { sm: 2 },
         pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) => alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-        }),
       }}
     >
-      {numSelected > 0 ? (
-        <Typography sx={{ flex: '1 1 100%' }} color="inherit" variant="subtitle1" component="div">
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography sx={{ flex: '1 1 100%' }} variant="h6" id="tableTitle" component="div">
-          Speakers
-        </Typography>
-      )}
+      <Typography sx={{ flex: '1 1 100%' }} variant="h6" id="tableTitle" component="div">
+        Speakers
+      </Typography>
     </Toolbar>
   );
-}
+};
 
-export default function SpeakerTable(props: { speakers: ISpeaker[] }) {
+const DynamicPopUp = dynamic(() => import('./PopUp'), { ssr: false });
+
+const SpeakerTable = (props: { speakers: ISpeaker[] }) => {
   const [order, setOrder] = useState<Order>('asc');
-  const [selected, setSelected] = useState<readonly string[]>([]);
+  const [orderBy, setOrderBy] = useState<keyof ISpeaker>('name');
+
+  const [selectedSpeaker, setSelectedSpeaker] = useState<ISpeaker>();
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [speakerDetailsPopup, setSpeakerDetailsPopup] = useState<boolean>(false);
 
-  const handleRequestSort = () => {
+  const handleRequestSort = (_: any, property: keyof ISpeaker) => {
     const isAsc = order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
   };
 
-  const handleSelectAllClick = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelected = props.speakers.map((n) => n.name);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event: any, name: string) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected: readonly string[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
-    }
-
-    setSelected(newSelected);
+  const handleClick = (speaker: ISpeaker) => {
+    setSelectedSpeaker(speaker);
+    setSpeakerDetailsPopup(true);
   };
 
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -178,102 +170,97 @@ export default function SpeakerTable(props: { speakers: ISpeaker[] }) {
     setPage(0);
   };
 
-  const isSelected = (name: string) => selected.indexOf(name) !== -1;
-
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - props.speakers.length) : 0;
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2 }}>
-        <SpeakerTableToolbar numSelected={selected.length} />
-        <TableContainer>
-          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size={'medium'}>
-            <SpeakerTableHead
-              numSelected={selected.length}
-              order={order}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={props.speakers.length}
-            />
-            <TableBody>
-              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+    <>
+      <Box sx={{ width: '100%' }}>
+        <Paper sx={{ width: '100%', mb: 2 }}>
+          <SpeakerTableToolbar />
+          <TableContainer>
+            <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size={'medium'}>
+              <SpeakerTableHead
+                order={order}
+                orderBy={orderBy}
+                onRequestSort={handleRequestSort}
+                rowCount={props.speakers.length}
+              />
+              <TableBody>
+                {/* if you don't need to support IE11, you can replace the `stableSort` call with:
               rows.sort(getComparator(order, orderBy)).slice() */}
-              {stableSort(props.speakers, order)
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((speaker, index) => {
-                  const isItemSelected = isSelected(speaker.name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
-
-                  return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, speaker.name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={speaker.name}
-                      selected={isItemSelected}
-                    >
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            'aria-labelledby': labelId,
-                          }}
-                        />
-                      </TableCell>
-                      <TableCell component="th" id={labelId} scope="row" padding="none">
-                        {speaker.name}
-                      </TableCell>
-                      <TableCell align="right">{speaker.listId ? speaker.listId : 'Create a list'}</TableCell>
-                      <TableCell align="right">
-                        <div
-                          style={{
-                            borderRadius: '2px',
-                            overflow: 'hidden',
-                            position: 'relative',
-                            width: 50,
-                            height: 50,
-                            backgroundImage: `url(${'/user.png'})`,
-                            backgroundPosition: 'center center',
-                            backgroundSize: 'cover',
-                          }}
-                        >
-                          {speaker.images?.find((image) => image.type === 'square') && (
-                            <Image
-                              src={speaker.images.find((image) => image.type === 'square')!.downloadLink}
-                              layout="fill"
-                            />
-                          )}
-                        </div>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: 53 * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={props.speakers.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </Box>
+                {stableSort(props.speakers, order, orderBy)
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((speaker) => {
+                    return (
+                      <TableRow hover onClick={() => handleClick(speaker)} tabIndex={-1} key={speaker.name}>
+                        <TableCell component="th" id={speaker.name} scope="row" padding="none">
+                          {speaker.name}
+                        </TableCell>
+                        <TableCell>{speaker.listId ? speaker.listId : 'No list'}</TableCell>
+                        <TableCell align="right">
+                          <div
+                            style={{
+                              borderRadius: '2px',
+                              overflow: 'hidden',
+                              position: 'relative',
+                              width: 50,
+                              height: 50,
+                              backgroundImage: `url(${'/user.png'})`,
+                              backgroundPosition: 'center center',
+                              backgroundSize: 'cover',
+                            }}
+                          >
+                            {speaker.images?.find((image) => image.type === 'square') && (
+                              <Image
+                                src={speaker.images.find((image) => image.type === 'square')!.downloadLink}
+                                layout="fill"
+                              />
+                            )}
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                {emptyRows > 0 && (
+                  <TableRow
+                    style={{
+                      height: 53 * emptyRows,
+                    }}
+                  >
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={props.speakers.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </Box>
+      <DynamicPopUp title="Speaker Details" open={speakerDetailsPopup} setOpen={setSpeakerDetailsPopup}>
+        <div>
+          <h2>{selectedSpeaker?.name}</h2>
+          <h2>Images:</h2>
+          {selectedSpeaker?.images
+            ? selectedSpeaker?.images?.map((image) => {
+                return (
+                  // eslint-disable-next-line @next/next/no-img-element
+                  <img key={selectedSpeaker?.name} src={image.downloadLink} width="200px" height="auto" />
+                );
+              })
+            : 'add an image'}
+        </div>
+      </DynamicPopUp>
+    </>
   );
-}
+};
+
+export default SpeakerTable;
