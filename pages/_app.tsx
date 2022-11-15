dd7613bbc29398b01e5567aaf0911c1689e6935c
Mina Hanna <53411598+mhanna15@users.noreply.github.com>
2022-11-15 09:42:49 -0800

    added loading between page changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { AudioPlayerProvider } from '../context/audio/audioPlayerContext';
 import { UserProvider } from '../context/user/UserContext';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
   return (
     <UserProvider>
       <Navbar />
       <AudioPlayerProvider>
+        <NextNProgress />
         <Component {...pageProps} />
       </AudioPlayerProvider>
       <Footer />
