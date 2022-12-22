9d8865bf5d16550f98a6d066ed8833fd9a471a27
Youssef Asaad <youssef.a.asaad@gmail.com>
2022-12-21 21:52:34 -0500

    added error handeler

---
+++ functions/src/handleError.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosError } from 'axios';
+import { HttpsError } from 'firebase-functions/v2/https';
+
+const handleError = (error: unknown): never => {
+  if (error instanceof HttpsError) {
+    throw error;
+  }
+  if (axios.isAxiosError(error)) {
+    const axiosError = error as AxiosError;
+    throw new HttpsError('internal', axiosError.message, axiosError.toJSON());
+  }
+  if (error instanceof Error) {
+    throw new HttpsError('internal', error.message);
+  }
+  throw new HttpsError('internal', 'Unknown error');
+};
+
+export default handleError;
