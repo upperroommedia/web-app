1dad219870c514f0c7ea09131f773a972977b852
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-02-11 20:44:06 -0500

    using url for v2 functions (uploadtosoundcloud)

--- utils/createFunction.ts
+++ utils/createFunction.ts
@@ -1,6 +1,10 @@
-import functions, { httpsCallable } from '../firebase/functions';
-
+import functions, { httpsCallable, httpsCallableFromURL } from '../firebase/functions';
 export const createFunction = <T = any, R = any>(name: string): ((data: T) => Promise<R>) => {
   const callable = httpsCallable<T, R>(functions, name);
+  return async (data: T) => (await callable(data)).data;
+};
+
+export const createFunctionV2 = <T = any, R = any>(name: string): ((data: T) => Promise<R>) => {
+  const callable = httpsCallableFromURL<T, R>(functions, `https://${name}-yshbijirxq-uc.a.run.app`);
   return async (data: T) => (await callable(data)).data;
 };
