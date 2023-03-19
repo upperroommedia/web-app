c443ac9931eb2956ca19f1e09b6f10e940b4ce12
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-03-19 18:46:43 -0400

    fixed storage path

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,6 +2,6 @@ import { Sermon } from '../types/SermonTypes';
 
 export function getSquareImageStoragePath(sermon: Sermon) {
   const imageId = sermon.images.find((image) => image.type === 'square')?.id;
-  const imageStoragePath = imageId ? `speaker-images/${imageId}` : undefined;
+  const imageStoragePath = imageId ? `speaker-images/${imageId}-square.jpeg` : undefined;
   return imageStoragePath;
 }
