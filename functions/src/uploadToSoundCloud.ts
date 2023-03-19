75bc3f4e6efbaa62f536ac7ad9874fe6b658b6ec
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-03-19 17:20:09 -0400

    update uploadtosoundcloud to use storage paths

--- functions/src/uploadToSoundCloud.ts
+++ functions/src/uploadToSoundCloud.ts
@@ -1,12 +1,11 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import FormData from 'form-data';
 import handleError from './handleError';
 import { CallableRequest, HttpsError, onCall } from 'firebase-functions/v2/https';
 import { logger } from 'firebase-functions/v2';
 
 export interface UploadToSoundCloudInputType {
-  audioUrl: string;
-  imageUrl?: string;
+  audioStoragePath: string;
+  imageStoragePath: string;
   title: string;
   speakers: string[];
   tags: string[];
@@ -17,58 +16,27 @@ export type UploadToSoundCloudReturnType = {
   soundCloudTrackId: string;
 };
 
-const uploadToSoundCloud = async ({
-  audioUrl,
-  imageUrl,
-  title,
-  speakers,
-  tags,
-  description,
-}: UploadToSoundCloudInputType): Promise<string> => {
-  const formData = new FormData();
-  const titleWithSpeakers = `${title} (${speakers.join(', ')})`;
-  const reformatedTags = tags.map((tag) => `"${tag}"`).join(' ');
-  const audioImageFiles = await Promise.all(
-    [audioUrl, imageUrl].map(async (url) => {
-      if (!url) {
-        return null;
-      }
-      const config: AxiosRequestConfig = { responseType: 'arraybuffer' };
-      const response = await axios.get(url, config);
-      return Buffer.from(response.data);
-    })
-  );
-
-  formData.append('title', titleWithSpeakers);
-  audioImageFiles[0] && formData.append('audio', audioImageFiles[0], { filename: 'audio.mp3' });
-  audioImageFiles[1] && formData.append('image', audioImageFiles[1], { filename: 'image.jpg' });
-  formData.append('tags', reformatedTags);
-  formData.append('description', description);
-  logger.log('formData Headers', JSON.stringify(formData.getHeaders()));
-  const config: AxiosRequestConfig = {
-    method: 'POST',
-    url: 'https://hook.eu1.make.com/q1tpo6rfktqe3a8cexb79n70j6sq0y65',
-    headers: {
-      ...formData.getHeaders(),
-    },
-    data: formData,
-  };
-  try {
-    return (await axios(config)).data;
-  } catch (error) {
-    throw handleError(error);
-  }
-};
-
 const uploadToSoundCloudCall = onCall(
   async (request: CallableRequest<UploadToSoundCloudInputType>): Promise<UploadToSoundCloudReturnType> => {
     logger.log('uploadToSoundCloud', request);
     if (request.auth?.token.role !== 'admin') {
       throw new HttpsError('unauthenticated', 'The function must be called while authenticated.');
     }
+    const data = request.data;
+    const reformatedTags = data.tags.map((tag) => `"${tag}"`).join(' ');
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: 'https://hook.eu1.make.com/q1tpo6rfktqe3a8cexb79n70j6sq0y65',
+      data: {
+        title: data.title,
+        audioStoragePath: data.audioStoragePath,
+        imageStoragePath: data.imageStoragePath,
+        tags: reformatedTags,
+        description: data.description,
+      },
+    };
     try {
-      logger.log('Attempting to upload to SoundCloud', request.data);
-      const response = await uploadToSoundCloud(request.data);
+      const response = (await axios(config)).data;
       logger.log('SoundCloud response', response);
       return { soundCloudTrackId: response };
     } catch (error) {
