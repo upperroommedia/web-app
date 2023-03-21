09d7e3627d7f52adde6b644827b32b705d5ba15f
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-03-20 22:42:11 -0400

    adding cloud-run backend code

---
+++ youtube-to-mp3-cloud-run/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  if (!req.query.url) {
+    res.status(500).send('No url query parameter provided');
+  }
+
+  // Apply the template to the parameters to generate an HTML string.
+  try {
+    ytdl(req.query.url, {
+      filter: 'audioonly',
+      quality: 'highestaudio',
+    }).pipe(res);
+  } catch (e) {
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(
+    `Hello from Cloud Run! The container started successfully and is listening for HTTP requests on ${PORT}`
+  );
+});
