4bd1db3d24cd7de7b6b913a9ced2d580c93ba430
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-03-21 01:15:22 -0400

    remove firebase auth

--- youtube-to-mp3-cloud-run/index.js
+++ youtube-to-mp3-cloud-run/index.js
@@ -1,19 +1,7 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const cors = require('cors');
-const {initializeApp, cert} = require('firebase-admin/app');
-const {getAuth} = require('firebase-admin/auth');
-const privateKey = JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY);
-const clientEmail = process.env.FIREBASE_ADMIN_CLIENT_EMAIL;
-const projectId = process.env.FIREBASE_PROJECT_ID;
 
-const firebaseApp = initializeApp({
-  credential: cert({
-    projectId,
-    privateKey,
-    clientEmail,
-  }),
-});
 const app = express();
 app.use(
   cors({
@@ -22,19 +10,8 @@ app.use(
 );
 
 app.get('/', async (req, res) => {
-  if (!req.headers.authorization) {
-    res.status(401).set('No authorization token was sent');
-  }
-
   if (!req.query.url) {
     res.status(500).send('No url query parameter provided');
-  }
-  try {
-    await getAuth(firebaseApp).verifyIdToken(
-      req.headers.authorization.split(' ')[1]
-    );
-  } catch (_error) {
-    res.status(401).set('User is not authenticated');
   }
   try {
     res.contentType('audio/mpeg');
