ecf308e8a4c16e8d0874e4e438a5f3f434b8b353
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-03-23 14:16:45 -0400

    Fix incorrect imageStoragePath

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,7 +1,10 @@
 import { Sermon } from '../types/SermonTypes';
 
 export function getSquareImageStoragePath(sermon: Sermon) {
-  const imageId = sermon.images.find((image) => image.type === 'square')?.id;
-  const imageStoragePath = imageId ? `speaker-images/${imageId}-square.jpeg` : undefined;
+  const encodedPath = sermon.images
+    .find((image) => image.type === 'square')
+    ?.downloadLink.split('/')
+    .pop();
+  const imageStoragePath = encodedPath ? decodeURIComponent(encodedPath) : undefined;
   return imageStoragePath;
 }
