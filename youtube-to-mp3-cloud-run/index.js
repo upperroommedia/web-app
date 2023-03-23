0ac653b226ee54146a98df9870fb589ad4e4879e
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-03-23 15:51:19 -0400

    ytdl with ffmpeg for audio encoding

--- youtube-to-mp3-cloud-run/index.js
+++ youtube-to-mp3-cloud-run/index.js
@@ -1,11 +1,31 @@
+/* eslint-disable no-console */
+const ffmpeg = require('fluent-ffmpeg');
 const express = require('express');
 const ytdl = require('ytdl-core');
 const cors = require('cors');
+const winston = require('winston');
+const expressWinston = require('express-winston');
 
 const app = express();
 app.use(
   cors({
     origin: '*',
+  })
+);
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: 'HTTP {{req.method}} {{req.url}}', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (_req, _res) {
+      return false;
+    }, // optional: allows to skip some log messages based on request and/or response
   })
 );
 
@@ -14,15 +34,35 @@ app.get('/', async (req, res) => {
     res.status(500).send('No url query parameter provided');
   }
   try {
+    console.log('URL', req.query.url);
     res.contentType('audio/mpeg');
-    ytdl(req.query.url, {
+    ytdl();
+    const audio = ytdl(req.query.url, {
       filter: 'audioonly',
       quality: 'highestaudio',
-      format: {
-        audioCodec: 'libmp3lame',
-      },
-    }).pipe(res);
+    });
+    ffmpeg(audio)
+      .audioCodec('libmp3lame')
+      .audioBitrate(128)
+      .format('mp3')
+      .on('start', function (commandLine) {
+        console.info(
+          'Trim And Transcode Spawned Ffmpeg with command: ' + commandLine
+        );
+      })
+      .on('progress', function (progress) {
+        console.info('Processing: ', progress);
+      })
+      .on('error', (err) => {
+        console.error('FFMPG ERROR', err);
+        res.status(500).send('Internal Server Error', err);
+      })
+      .on('end', () => console.log('Finished!'))
+      .pipe(res, {
+        end: true,
+      });
   } catch (e) {
+    console.error(e);
     res.status(500).send('Internal Server Error');
   }
 });
