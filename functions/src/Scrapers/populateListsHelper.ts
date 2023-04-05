bdc5e805e419934a6d2897f457248353158cf0b9
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-04-04 22:57:45 -0400

    update list category types

--- functions/src/Scrapers/populateListsHelper.ts
+++ functions/src/Scrapers/populateListsHelper.ts
@@ -8,6 +8,42 @@ import populateImages from './populateImagesHelper';
 import { Bucket } from '@google-cloud/storage';
 import { HttpsError } from 'firebase-functions/v2/https';
 
+async function updateCategories(
+  db: firestore.Firestore,
+  bearerToken: string,
+  firestoreLists: firestore.CollectionReference<List>
+) {
+  // update categories lists
+  const categoriesResponse = (
+    await axios(
+      createAxiosConfig(
+        'https://core.subsplash.com/builder/v1/list-rows?filter%5Bapp_key%5D=9XTSHD&filter%5Bsource_list%5D=c305f7c7-f299-4db9-bf4a-936e79061739&include=images&page%5Bnumber%5D=1&page%5Bsize%5D=100&sort=position',
+        bearerToken,
+        'GET',
+        undefined
+      )
+    )
+  ).data;
+  const listRows = categoriesResponse._embedded['list-rows'];
+  const batch = db.batch();
+  listRows.forEach((listRow: any) => {
+    const listId = listRow._embedded.list.id;
+    if (!listId) {
+      throw new Error("ListId for category doesn't exist");
+    }
+    logger.log(`Updating list ${listId} to be a category list - ${listRow._embedded.list.title}`);
+    batch.update(firestoreLists.doc(listId), { type: ListType.CATEGORY_LIST });
+  });
+
+  // update latest list
+  batch.update(firestoreLists.doc('7b7d3fc9-8600-4ec0-b3d1-eed79c0a2ac6'), {
+    type: ListType.LATEST,
+    overflowBehavior: OverflowBehavior.REMOVEOLDEST,
+  });
+
+  await batch.commit();
+}
+
 async function populateLists(
   db: firestore.Firestore,
   bucket: Bucket,
@@ -108,6 +144,7 @@ async function populateLists(
     });
     await batch.commit();
   }
+  await updateCategories(db, bearerToken, firestoreLists);
   console.timeEnd('Write Lists To Firestore');
   logger.log(`There are ${listNameToId.size} unique lists`);
   return current;
