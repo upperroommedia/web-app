a5d768f4a42ac56063da83fb8949c3bf62a042b3
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-06-02 02:09:52 -0400

    fix package dependencies

--- functions/src/Scrapers/populateDatabaseFromSubsplash.ts
+++ functions/src/Scrapers/populateDatabaseFromSubsplash.ts
@@ -14,7 +14,8 @@ import populateSpeakers from './populateSpeakersHelper';
 import populateTopics from './populateTopicsHelper';
 import handleError from '../handleError';
 
-const { firestore, storage } = firebaseAdmin;
+const storage = firebaseAdmin.storage();
+const firestore = firebaseAdmin.firestore();
 export interface populateDatabaseFromSubsplashInputType {
   speakerTagIds?: string[];
 }
@@ -38,8 +39,8 @@ const populateDatabaseFromSubsplash = onCall(
       const listIdToImageIdMap = new Map<string, string[]>();
       const listNameToId = new Map<string, string>();
       const bearerToken = await authenticateSubsplash();
-      const bucket = storage().bucket('urm-app-images');
-      const db = firestore();
+      const bucket = storage.bucket('urm-app-images');
+      const db = firestore;
       const firestoreLists = db.collection('lists').withConverter(firestoreAdminListConverter);
       const firestoreSpeakers = db.collection('speakers').withConverter(firestoreAdminSpeakerConverter);
       const firestoreTopics = db.collection('topics').withConverter(firestoreAdminTopicConverter);
