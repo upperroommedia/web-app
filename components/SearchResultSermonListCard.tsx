4aba9696f164e1086f29ffd8cc5e7bf13ba53439
Youssef Asaad <youssef.a.asaad@gmail.com>
2023-10-10 17:16:39 -0400

    Added SearchResultSermonListCard

---
+++ components/SearchResultSermonListCard.tsx
@@ -0,0 +1,48 @@
+import firestore, { doc } from '../firebase/firestore';
+import { FunctionComponent } from 'react';
+import { useDocument } from 'react-firebase-hooks/firestore';
+import { sermonConverter } from '../types/Sermon';
+import SermonListCardSkeloten from './skeletons/SermonListCardSkeloten';
+import SermonListCard from './SermonListCard';
+import { Sermon } from '../types/SermonTypes';
+import { SermonWithMetadata } from '../reducers/audioPlayerReducer';
+
+interface SearchResultSermonListCardProps {
+  sermonId: string;
+  playing: boolean;
+  playlist: SermonWithMetadata[];
+  setPlaylist: (playlist: Sermon[]) => void;
+  currentSecond: number;
+  minimal?: boolean;
+}
+
+const SearchResultSermonListCard: FunctionComponent<SearchResultSermonListCardProps> = ({
+  sermonId,
+  playing,
+  playlist,
+  setPlaylist,
+  currentSecond,
+  minimal,
+}) => {
+  const [sermon, loading, error] = useDocument(doc(firestore, `sermons/${sermonId}`).withConverter(sermonConverter), {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  });
+
+  return (
+    <>
+      {error && <strong>Error: {JSON.stringify(error)}</strong>}
+      {loading && <SermonListCardSkeloten />}
+      {sermon && sermon.exists() && (
+        <SermonListCard
+          sermon={{ ...sermon.data(), currentSecond }}
+          playing={playing}
+          playlist={playlist}
+          setPlaylist={setPlaylist}
+          minimal={minimal}
+        />
+      )}
+    </>
+  );
+};
+
+export default SearchResultSermonListCard;
