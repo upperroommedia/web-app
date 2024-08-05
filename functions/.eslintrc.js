6eac8cbb22029c6a5a684df17677a8fe18d39c76
Youssef Asaad <youssef.a.asaad@gmail.com>
2024-08-05 12:35:20 +0300

    fix eslint error

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    // project: ['tsconfig.json', 'tsconfig.dev.json'],
     sourceType: 'module',
   },
   ignorePatterns: [
