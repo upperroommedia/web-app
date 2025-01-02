c6ee06f867203f8d5c2c9cb252afc3bd7e6e6258
Mina Hanna <minaghanna@gmail.com>
2025-01-02 15:48:30 -0800

    Revert "added a feedback link"
    This reverts commit 2b913ef406f7c5e1145c8d84d5721a926c397017.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,29 +2,23 @@
  * Footer located at the bottom of all pages
  */
 import Image from 'next/image';
-import Link from 'next/link';
 import { FunctionComponent } from 'react';
 import styles from '../styles/Footer.module.css';
 
 const Footer: FunctionComponent = () => (
   <footer className={styles.footer}>
-    <Link href="https://forms.gle/5Nw35vnq6JUkHEe59" target="_blank" rel="noopener noreferrer" className={styles.link}>
-      Give us feedback!
-    </Link>
-    <div>
-      <p>
-        Powered by{' '}
-        <span className={styles.logo}>
-          <Image
-            src="/coptic-devs-logo.webp"
-            alt="Coptic Devs Logo"
-            fill
-            sizes="36px, 36px"
-            style={{ objectFit: 'cover' }}
-          />
-        </span>
-      </p>
-    </div>
+    <p>
+      Powered by{' '}
+      <span className={styles.logo}>
+        <Image
+          src="/coptic-devs-logo.webp"
+          alt="Coptic Devs Logo"
+          fill
+          sizes="36px, 36px"
+          style={{ objectFit: 'cover' }}
+        />
+      </span>
+    </p>
   </footer>
 );
 
