0eb0a5a01496efececbe5a59acefb066f1418e92
Youssef Asaad <youssef.asaad@hadrian.co>
2025-06-03 16:58:33 -0700

    Removed give us feedback link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,15 +2,11 @@
  * Footer located at the bottom of all pages
  */
 import Image from 'next/image';
-import Link from 'next/link';
 import { FunctionComponent } from 'react';
 import styles from '../styles/Footer.module.css';
 
 const Footer: FunctionComponent = () => (
   <footer className={styles.footer}>
-    <Link href="https://forms.gle/5Nw35vnq6JUkHEe59" target="_blank" rel="noopener noreferrer" className={styles.link}>
-      Give us feedback!
-    </Link>
     <div>
       <p>
         Powered by{' '}
