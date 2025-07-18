a8190d5d379b7a3a37f82eb5b0348ddc3b887a9a
Youssef Asaad <youssef.a.asaad@gmail.com>
2025-07-17 19:25:23 -0700

    feat: add confirmation popup to delete from soundcloud

--- components/PopUp.tsx
+++ components/PopUp.tsx
@@ -1,5 +1,5 @@
 import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
+import DialogActions, { type DialogActionsProps } from '@mui/material/DialogActions';
 import Dialog, { DialogProps } from '@mui/material/Dialog';
 import Button from '@mui/material/Button';
 import DialogTitle from '@mui/material/DialogTitle';
@@ -11,9 +11,11 @@ interface PopUpInfo {
   children: string | ReactElement<any>;
   setOpen: (setOpen: boolean) => void;
   button?: ReactElement<any>;
+  disabled?: boolean;
   onClose?: () => void;
   // optional props to pass to Dialog
   dialogProps?: Omit<DialogProps, 'open'>;
+  dialogActionsProps?: DialogActionsProps;
 }
 
 const PopUp = (props: PopUpInfo) => {
@@ -28,8 +30,8 @@ const PopUp = (props: PopUpInfo) => {
     <Dialog open={open} onClose={onClose} {...props.dialogProps} aria-labelledby="confirm-dialog">
       {title && <DialogTitle id="confirm-dialog">{title}</DialogTitle>}
       <DialogContent>{children}</DialogContent>
-      <DialogActions>
-        <Button variant="contained" onClick={onClose} color="primary">
+      <DialogActions {...props.dialogActionsProps}>
+        <Button variant="contained" onClick={onClose} color="primary" disabled={props.disabled}>
           {props.button ? 'Cancel' : 'Close'}
         </Button>
         {props.button}
