b1a0f543f118a1c95db361e7c10d0c43900431c6
Youssef Asaad <youssef.a.asaad@gmail.com>
2026-04-06 23:10:27 -0700

    sentry visibility updates

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -4,6 +4,9 @@ import { webSentryConfig, webTracePropagationTargets } from './sentry.shared';
 Sentry.init({
   ...webSentryConfig,
   sendDefaultPii: false,
-  integrations: [Sentry.replayIntegration()],
+  integrations: [Sentry.replayIntegration({
+    maskAllText: false,
+    blockAllMedia: false,
+  })],
   tracePropagationTargets: webTracePropagationTargets,
 });
